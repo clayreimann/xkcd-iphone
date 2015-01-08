8f0b6e83cd3a25c36fbddc15d5bb5256dc0b2151
Clay Reimann <clayreimann@gmail.com>
2015-01-08 07:21:59 -0600

    Initial Commit

---
+++ xkcd/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  xkcd
+//
+//  Created by Christopher Reimann on 1/8/15.
+//  Copyright (c) 2015 Clay Reimann. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ xkcd/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  xkcd
+//
+//  Created by Christopher Reimann on 1/8/15.
+//  Copyright (c) 2015 Clay Reimann. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
